44823cc8e1ec77c50924527e8cbbc0c7904bb717
Katelyn Yaeger <knyaeger93@gmail.com>
2022-08-31 16:33:43 -0500

    finished modifications

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -13,11 +13,11 @@ namespace ChristmasWithTypes
             //Done set Santa's name to Kris Kringle
             xmas.Santa = "Kris Kringle";
 
-            //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{"book" "purse" ""}; 
+            //Done Insert 3 presents you would like for xmas.  They must be strings
+            xmas.Presents =  new string[3]{"Xbox", "iphoneX", "Nintendo Switch"}; 
 
-            //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            //Done Set the TreeHeight to 10
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
